fbca69c0edbaac263508e75803d87fecba37ff59
dipak-wagh <dipakwagh0502@gmail.com>
2026-01-24 01:55:39 +0530

    Added today's programs

---
+++ Function.py
@@ -0,0 +1,8 @@
+def happy_birthday(name,age):
+    print(f"Happy birthday to {name}!")
+    print(f"You are {age} years old!")
+    print("Happy birthday to you!")
+    print()
+
+happy_birthday("Bro",20)
+happy_birthday("steve",30)
---
+++ FunctionReturn.py
@@ -0,0 +1,21 @@
+
+def add(x,y):
+    z=x+y
+    return z
+
+def sub(x,y):
+    z=x-y
+    return z
+
+def mult(x,y):
+    z=x*y
+    return z
+
+def div(x,y):
+    z=x/y
+    return z
+
+print(add(1,4))
+print(sub(13,5))
+print(mult(12,3))
+print(div(22,4))
